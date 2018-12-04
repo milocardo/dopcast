6724a59afcc001aaa681369e7f0326262eafa3e7
Ricardo Montes <tognolamontes@yahoo.com.br>
2018-12-04 16:31:55 -0200

    created episode and playlist controller

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,2 +1,18 @@
 class EpisodesController < ApplicationController
+  def index
+    @episode = Episode.all
+  end
+
+  def show
+    @episode = Episode.find(params[:id])
+  end
+
+  def new
+    @episode = Episode.new
+  end
+
+  def create
+    @episode = Episode.new(params[:episode])
+    @episode.save
+  end
 end
--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,43 @@
 class PlaylistsController < ApplicationController
+  before_action :set_playlist, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @playlist = Playlist.all
+  end
+
+  def show
+    set_playlist
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+    @playlist.save
+    redirect_to playlist_path(@playlist)
+  end
+
+  def update
+    set_playlist
+    @playlist.update(playlist_params)
+    redirect_to playlists_path(@playlist)
+  end
+
+  def destroy
+    set_playlist
+    @playlist.destroy
+    redirect_to playlists_path
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :address)
+  end
+
+  def set_playlist
+    @playlist = Playlist.find(params[:id])
+  end
 end
