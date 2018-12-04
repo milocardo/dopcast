c768535ef6eeda69981e3cb13fe9623602eabee4
Ricardo Montes <tognolamontes@yahoo.com.br>
2018-12-04 14:57:27 -0200

    correct the db

--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 2018_12_03_201602) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "country"
-    t.string "playlist"
     t.string "subscription"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
