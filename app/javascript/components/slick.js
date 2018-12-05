3e75f52c36621ec4bd70088ca253373143d4bc2f
Ricardo Montes <tognolamontes@yahoo.com.br>
2018-12-05 19:45:58 -0200

    added the slick carousel into the podcast index page

---
+++ app/javascript/components/slick.js
@@ -0,0 +1,33 @@
+import 'slick-carousel'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+function initSlick() {
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+}
+
+export { initSlick };
