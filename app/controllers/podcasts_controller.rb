a293f9a5ca8c653d6576e8b3c6124e32548ea200
Ricardo Montes <tognolamontes@yahoo.com.br>
2018-12-06 10:45:00 -0200

    fix podcast controller

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,13 +1,6 @@
-      require 'json'
-
 class PodcastsController < ApplicationController
   def index
     if params[:query].present?
-      filepath = "https://itunes.apple.com/search?term=#{params[:query]}"
-      podcasts_info = File.read(filepath)
-      podcasts = JSON.parse(podcasts_info)
-
-      raise
 
       PgSearch::Multisearch.rebuild(Podcast)
       PgSearch::Multisearch.rebuild(Episode)
