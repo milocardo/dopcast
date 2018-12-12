8acdce9d513d085c389dd4e1677cc28042ce7dde
Ricardo Montes <tognolamontes@yahoo.com.br>
2018-12-12 16:19:40 -0200

    added the type search funcionality

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -3,14 +3,16 @@ require 'open-uri'
 
 class PodcastsController < ApplicationController
   def index
-    if params[:query].present?
-      # Search all the podcasts with the query params
+    if params[:query][:types].include?("podcast") || params[:query][:types] == [""]
       @podcasts = HTTParty.get(
-       "https://listennotes.p.mashape.com/api/v1/search?type=podcast&q=#{params[:query]}",
+        "https://listennotes.p.mashape.com/api/v1/search",
         headers: {
           "X-Mashape-Key" => ENV['FULL_SEARCH_KEY'],
           "Accept" => "application/json"
-        }
+        },
+        query: {
+          type: 'podcast',
+        }.merge(query_params)
       )
 
       @podcasts["results"].each do |podcast_info|
@@ -25,7 +27,7 @@ class PodcastsController < ApplicationController
             "Accept" => "application/json"
           }
         )
-        #If the podcast doesnt exist yet, create a new podcast
+          #If the podcast doesnt exist yet, create a new podcast
         podcast.update!(
           total_episodes: response["total_episodes"],
           itunes_id: response["itunes_id"],
@@ -42,8 +44,11 @@ class PodcastsController < ApplicationController
           extra: response["extra"]
         )
       end
-      #Search for all the episodes with the query params
-      @episodes = HTTParty.get( "https://listennotes.p.mashape.com/api/v1/search?type=episode&q=#{params[:query]}",
+      @podcasts = PgSearch.multisearch(params[:query][:q]).where(:searchable_type => "Podcast")
+    end
+
+    if params[:query][:types].include?("episode") || params[:query][:types] == [""]
+      @episodes = HTTParty.get( "https://listennotes.p.mashape.com/api/v1/search?type=episode&q=#{params[:query][:q]}",
         headers:{
           "X-Mashape-Key" => ENV['FULL_SEARCH_KEY'],
           "Accept" => "application/json"
@@ -51,7 +56,6 @@ class PodcastsController < ApplicationController
       )
 
       @episodes["results"].each do |episode_info|
-    # binding.pry
         episode = Episode.find_or_initialize_by(korean_episode_id: episode_info['id'])
         next if episode.persisted?
         #Search for the information of the podcast which the episode is nested on
@@ -86,9 +90,7 @@ class PodcastsController < ApplicationController
             "Accept" => "application/json"
           }
         #If the podcast doesnt exist yet, create a new podcast
-        # podcast = Podcast.last
         episode.update!(
-          # podcast_id: podcast["id"],
           title: response["title"],
           audio_length: response["audio_length"],
           audio: response["audio"],
@@ -98,12 +100,111 @@ class PodcastsController < ApplicationController
           podcast: podcast
         )
       end
-      @podcasts = PgSearch.multisearch(params[:query]).where(:searchable_type => "Podcast")
-      @episodes = PgSearch.multisearch(params[:query]).where(:searchable_type => "Episode")
-    else
-      @podcasts = Podcast.all
+      @episodes = PgSearch.multisearch(params[:query][:q]).where(:searchable_type => "Episode")
     end
+    @playlists = PgSearch.multisearch(params[:query][:q]).where(:searchable_type => "Playlist")
   end
+    # if params.dig(:query, :q).present?
+    #   # Search all the podcasts with the query params
+    #   @podcasts = HTTParty.get(
+    #    "https://listennotes.p.mashape.com/api/v1/search?type=podcast&q=#{params[:query][:q]}",
+    #     headers: {
+    #       "X-Mashape-Key" => ENV['FULL_SEARCH_KEY'],
+    #       "Accept" => "application/json"
+    #     }
+    #   )
+
+    #   @podcasts["results"].each do |podcast_info|
+    #     #See if the podcast is on to the database
+    #     podcast = Podcast.find_or_initialize_by(itunes_id: podcast_info['itunes_id'])
+    #     next if podcast.persisted?
+    #     #Search for all the information off the specific podcast
+    #     response = HTTParty.get(
+    #       "https://listennotes.p.mashape.com/api/v1/podcasts/#{podcast_info['id']}?next_episode_pub_date=1479154463000&sort=recent_first",
+    #       headers: {
+    #         "X-Mashape-Key" => ENV['ESPECIFIC_SEARCH_KEY'],
+    #         "Accept" => "application/json"
+    #       }
+    #     )
+    #     #If the podcast doesnt exist yet, create a new podcast
+    #     podcast.update!(
+    #       total_episodes: response["total_episodes"],
+    #       itunes_id: response["itunes_id"],
+    #       image: response["image"],
+    #       title: response["title_original"],
+    #       episodes_list: response["episodes"],
+    #       country: response["country"],
+    #       description: response["description"],
+    #       language: response["language"],
+    #       lastest_pub_date_ms: response["lastest_pub_date_ms"],
+    #       earliest_pub_date_ms: response["earliest_pub_date_ms"],
+    #       publisher: response["publisher"],
+    #       genres: response["genres"],
+    #       extra: response["extra"]
+    #     )
+    #   end
+      #Search for all the episodes with the query params
+    #   @episodes = HTTParty.get( "https://listennotes.p.mashape.com/api/v1/search?type=episode&q=#{params[:query][:q]}",
+    #     headers:{
+    #       "X-Mashape-Key" => ENV['FULL_SEARCH_KEY'],
+    #       "Accept" => "application/json"
+    #     }
+    #   )
+
+    #   @episodes["results"].each do |episode_info|
+    # # binding.pry
+    #     episode = Episode.find_or_initialize_by(korean_episode_id: episode_info['id'])
+    #     next if episode.persisted?
+    #     #Search for the information of the podcast which the episode is nested on
+    #     podcast_info = HTTParty.get "https://listennotes.p.mashape.com/api/v1/podcasts/#{episode_info["podcast_id"]}",
+    #       headers:{
+    #         "X-Mashape-Key" => ENV['ESPECIFIC_SEARCH_KEY'],
+    #         "Accept" => "application/json"
+    #       }
+    #       podcast = Podcast.find_or_initialize_by(itunes_id: podcast_info['itunes_id'])
+    #       unless podcast.persisted?
+    #       #If the podcast doesnt exist yet, create a new podcast
+    #         podcast.update!(
+    #           total_episodes: podcast_info["total_episodes"],
+    #           itunes_id: podcast_info["itunes_id"],
+    #           image: podcast_info["image"],
+    #           title: podcast_info["title"],
+    #           episodes_list: podcast_info["episodes"],
+    #           country: podcast_info["country"],
+    #           description: podcast_info["description"],
+    #           language: podcast_info["language"],
+    #           lastest_pub_date_ms: podcast_info["lastest_pub_date_ms"],
+    #           earliest_pub_date_ms: podcast_info["earliest_pub_date_ms"],
+    #           publisher: podcast_info["publisher"],
+    #           genres: podcast_info["genres"],
+    #           extra: podcast_info["extra"]
+    #         )
+    #       end
+    #     #Search all the information of an specific episode
+    #     response = HTTParty.get "https://listennotes.p.mashape.com/api/v1/episodes/#{episode_info['id']}",
+    #       headers:{
+    #         "X-Mashape-Key" => ENV['ESPECIFIC_SEARCH_KEY'],
+    #         "Accept" => "application/json"
+    #       }
+    #     #If the podcast doesnt exist yet, create a new podcast
+    #     # podcast = Podcast.last
+    #     episode.update!(
+    #       # podcast_id: podcast["id"],
+    #       title: response["title"],
+    #       audio_length: response["audio_length"],
+    #       audio: response["audio"],
+    #       description: response["description"],
+    #       pub_date_ms: response["pub_date_ms"],
+    #       image: response["image"],
+    #       podcast: podcast
+    #     )
+    #   end
+  #     @podcasts = PgSearch.multisearch(params[:query][:q]).where(:searchable_type => "Podcast")
+  #     @episodes = PgSearch.multisearch(params[:query][:q]).where(:searchable_type => "Episode")
+  #   else
+  #     @podcasts = Podcast.all
+  #   end
+  # end
 
   def show
     @podcast = Podcast.find(params[:id])
@@ -203,5 +304,9 @@ class PodcastsController < ApplicationController
      :publisher,
      :genres,
      :extra)
+  end
+
+  def query_params
+    params.require(:query).permit(:q, :genre_ids, :language, :len_min, :len_max)
   end
 end
